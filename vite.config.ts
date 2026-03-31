7754f57e55da3dd64d85f190974e7caf8aec4ad2
Dieujuste <zdieujuste@quantaservices.com>
2026-03-31 13:51:30 -0400

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
